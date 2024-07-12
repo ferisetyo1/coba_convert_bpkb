e99d87b1eab4bc7f3d04b98a7fa5aa18f20e173e
Feri Setyo Efendi <ferisetyo1@gmail.com>
2024-07-12 17:02:50 +0700

    update

--- lib/baf_qr.dart
+++ lib/baf_qr.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/services.dart';
 
 class BAFQR {
-  static const MethodChannel _apiChannel = MethodChannel("ugsv_flutter");
+  static const MethodChannel _apiChannel = MethodChannel("baf_qr");
   static void openLogin() {
     _apiChannel.invokeMethod("open_login");
   }
